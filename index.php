2cfd477838687aea3adaf59b0d67d8ce08475a8b
josh <84872486+1justjosh@users.noreply.github.com>
2024-11-15 14:14:08 +0000

    avalanche home page

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/Logo.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Avalanche Interactive</title>
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <img class="logo" src="assets/Logo.png" alt="Avalanche Interactive Logo">
+
+            <ul class="links">
+                <li>
+                    <a class="selected" href="">Home</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#">About</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Comapny</a></li>
+                        <li><a href="#">Engine</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="">Store</a>
+                </li>
+                <li>
+                    <a href="">Resources</a>
+                </li>
+                <li>
+                    <a href="">Support</a>
+                </li>
+            </ul>
+
+            <div class="header-btn">
+                <a href="">Sign-Up</a>
+                <a href="">Login</a>
+            </div>
+        </div>
+    </nav>
+
+</body>
+
+</html>
